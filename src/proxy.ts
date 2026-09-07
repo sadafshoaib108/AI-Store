a74ed3fb92a7a25b99e834ab846a4eb1c92d36dc
Sadaf <sadafshoaib108@gmail.com>
2026-09-07 14:30:27 -0700

    Migrate middleware to proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,8 +1,8 @@
 import { createServerClient } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
 
-export async function middleware(request: NextRequest) {
-  let response = NextResponse.next({
+export async function proxy(request: NextRequest) {
+    let response = NextResponse.next({
     request,
   });
 
